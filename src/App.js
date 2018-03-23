47f50f91a99ee166f884fa921e2e3edab090be24
Josh Min <minje@usc.edu>
2018-03-22 21:32:22 -0700

    Added a looping animation to the coin.

--- src/App.js
+++ src/App.js
@@ -3,19 +3,27 @@ import 'aframe';
 import {Entity, Scene} from 'aframe-react';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Scene>
-          <Entity obj-model={{obj: './COIN.obj'}}
-                  position={{x: 0, y: 0, z: -7}}
-                  rotation={{x: 90, y: 10, z: 0}}
-                  scale={{x: 0.5, y: 0.5, z: 0.5}}
-                  material={{src: './COIN.png', metalness: '0.33', roughness: '0.22'}}/>
-        </Scene>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+              <Scene>
+                <Entity obj-model={{obj: './COIN.obj'}}
+                        position={{x: 0, y: 0, z: -7}}
+                        rotation={{x: 90, y: 10, z: 0}}
+                        scale={{x: 0.5, y: 0.5, z: 0.5}}
+                        material={{src: './COIN.png', metalness: '0.33', roughness: '0.22'}}>
+                  <a-animation attribute="rotation"
+                               dur="10000"
+                               fill="forwards"
+                               to="0 360 0"
+                               repeat="indefinite"></a-animation>
+                </Entity>
+
+                <a-sky color="#eee"></a-sky>
+              </Scene>
+            </div>
+        );
+    }
 }
 
 export default App;
